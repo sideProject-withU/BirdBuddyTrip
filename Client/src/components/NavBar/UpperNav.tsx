ecbeb4fd160e99798ba343eedc6f4d4281fbd4a3
sophiecode1105 <sophiecode1105@gmail.com>
2022-04-22 12:03:52 +0900

    Feat: 상단 네비게이션바 Select기능 구현

--- Client/src/components/NavBar/UpperNav.tsx
+++ Client/src/components/NavBar/UpperNav.tsx
@@ -1,12 +1,61 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.nav`
+  position: fixed;
+  max-width: 768px;
   height: 5rem;
-  background-color: rgb(196, 196, 196);
+  background-color: #007580;
+  top: 0;
+  width: 100%;
+`;
+
+const Selection = styled.select`
+  width: 5.5rem;
+  padding: 0.5em 0.5em;
+  font-weight: bold;
 `;
 
 const UpperNav = () => {
-  return <Container>높이보기</Container>;
+  const [choice, setChoice] = useState<string>('');
+  const [searchClick, setFind] = useState<boolean>(false);
+  const lists = [
+    '지역',
+    '서울',
+    '경기도',
+    '인천',
+    '강원도',
+    '대전',
+    '충청남도',
+    '충청북도',
+    '광주',
+    '전라남도',
+    '전라북도',
+    '대구',
+    '경상북도',
+    '경상남도',
+    '울산',
+    '부산',
+  ];
+
+  const options = lists.map((list, i) => {
+    return (
+      <option key={i} value={lists[i]}>
+        {list}
+      </option>
+    );
+  });
+
+  return (
+    <Container>
+      <Selection
+        onInput={(event: React.FormEvent<HTMLSelectElement>) => setChoice((event.target as HTMLInputElement).value)}
+      >
+        {options}
+      </Selection>
+      <div></div>
+    </Container>
+  );
 };
 
 export default UpperNav;
