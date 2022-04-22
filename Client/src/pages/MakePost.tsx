c7a53f5c62a645d0548d0f40007c52bdaa10287d
jiyuunyang <jiyuunyang@gmail.com>
2022-04-22 13:01:34 +0900

    Add: [Client] #9 주요 page 생성
    - Mypage
    - MakePost
    - ChatList

---
+++ Client/src/pages/MakePost.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 1px solid red;
+`;
+const MakePost = () => {
+  return <Container>make post</Container>;
+};
+export default MakePost;
