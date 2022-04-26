75a758ec6e18fb66f0d2a98f78ce1bc57336d58c
jiyuunyang <jiyuunyang@gmail.com>
2022-04-26 12:11:52 +0900

    Design: [Client] #30 Post컴포넌트 CSS 수정
    - Post Component CSS 수정
    - test용 photo추가(photo_test1.jpeg)

--- Client/src/components/Post/Post.tsx
+++ Client/src/components/Post/Post.tsx
@@ -1,42 +1,85 @@
 import styled from 'styled-components';
 export const Container = styled.article`
+  display: flex;
   height: 8rem;
-  border: 5px solid #d8ebe4;
+  border-bottom: 2px solid #789b9e;
+  background-color: #e3ede9;
   padding: 1rem;
-  > header {
-    font-size: 1.5rem;
+  &:hover {
+    background-color: #d8ebe4;
   }
-`;
-export const PeopleRegion = styled.div`
-  display: flex;
-  font-size: 1.2rem;
-`;
-export const JourneyDate = styled.div`
-  display: flex;
-  font-size: 1.2rem;
-`;
-export const Options = styled.div`
-  display: flex;
-  font-size: 1.2rem;
+  > .col-01 {
+    max-width: 6rem;
+    max-height: 6rem;
+    border-radius: 0.3rem;
+    overflow: hidden;
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  > .col-02 {
+    padding-left: 1rem;
+    max-width: 38rem;
+    > header {
+      margin: 0.3rem 0;
+      h1 {
+        font-weight: bold;
+        font-size: 1.3rem;
+        height: 1.3rem;
+        overflow: hidden;
+      }
+    }
+    > .people-region {
+      margin-bottom: 0.3rem;
+    }
+    > .date {
+      font-size: 0.8rem;
+      font-weight: bold;
+      margin-bottom: 0.3rem;
+    }
+    > .options {
+      > span {
+        border-radius: 0.3rem;
+        margin: 0 0.3rem 0 0;
+        background-color: #fed1496d;
+      }
+    }
+  }
+  > .col-03 {
+    padding-left: 1rem;
+    margin-left: auto;
+  }
 `;
 const Post = () => {
   return (
     <Container>
-      <header>
-        <h1>Post제목</h1>
-      </header>
-      <PeopleRegion>
-        <span>인원</span>
-        <span>지역</span>
-      </PeopleRegion>
-      <JourneyDate>
-        <span>시작날짜</span>
-        <span>종료날짜</span>
-      </JourneyDate>
-      <Options>
-        <span>태그1</span>
-        <span>태그2</span>
-      </Options>
+      <div className='col-01'>
+        <img src='images/photo_test1.jpeg' alt='photo_test1' />
+      </div>
+      <div className='col-02'>
+        <header>
+          <h1>Post제목이 길면어떻게 될까요오오오오오</h1>
+        </header>
+        <div className='date'>
+          <span>2022.03.01</span>
+          <span>-</span>
+          <span>2022.03.03</span>
+        </div>
+        <div className='people-region'>
+          <span>인원 1/10</span>
+          <span>경상북도</span>
+        </div>
+        <div className='options'>
+          <span>성별무관</span>
+          <span>익스트림</span>
+        </div>
+      </div>
+      <div className='col-03'>
+        <i className='fa-regular fa-heart'></i>
+        <i className='fa-solid fa-heart'></i>
+      </div>
     </Container>
   );
 };
