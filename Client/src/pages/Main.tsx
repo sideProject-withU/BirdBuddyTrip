b5ae7f4d017436e811c2ae08474bf83cba7d17f4
jiyuunyang <jiyuunyang@gmail.com>
2022-04-28 11:40:14 +0900

    Style: [Client] #58 main페이지 OptionContainer 조정

--- Client/src/pages/Main.tsx
+++ Client/src/pages/Main.tsx
@@ -2,12 +2,15 @@ import GenderOption from '../components/Options/GenderOption';
 import TypeOption from '../components/Options/TypeOption';
 import PostThumb from '../components/Post/PostThumb';
 import { dummyPosts } from '../dummys/dummyPosts';
+import { OptionContainer } from '../styled/options';
 
 const Main = () => {
   return (
     <div>
-      <GenderOption />
-      <TypeOption />
+      <OptionContainer>
+        <GenderOption />
+        <TypeOption />
+      </OptionContainer>
       {dummyPosts.map((el, idx) => (
         <PostThumb key={idx} post={el} />
       ))}
